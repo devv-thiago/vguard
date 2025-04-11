308d3998e77569cb40f6640898799bb3fdb0f565
devv-thiago <tfofano3@gmail.com>
2025-04-11 19:36:17 -0300

    Improving example file

--- example/vguard_example.dart
+++ example/vguard_example.dart
@@ -1,8 +1,32 @@
 import 'package:vguard/src/utils/extensions.dart';
 
 void main() {
-  final exampleString = "Example";
-  exampleString.vguard.isEmail();
-  exampleString.vguard.isMaximumLength(10);
-  
+  final email = "user@example.com";
+  final password = "MyP@ssw0rd!";
+
+  final emailResult = email.vguard
+      .isRequired(errorMessage: "Email is required")
+      .isEmail(errorMessage: "Invalid email format")
+      .run();
+
+  final passwordResult = password.vguard
+      .isRequired(errorMessage: "Password is required")
+      .isMinimumLength(8,
+          errorMessage: "Password must be at least 8 characters")
+      .isSecurePassword(
+          highSecurityValidation: false,
+          errorMessage: "Password must contain letters, numbers, and symbols")
+      .run();
+
+  if (!emailResult.isValid) {
+    print("Email error: ${emailResult.errors}");
+  } else {
+    print('Valid e-mail');
+  }
+
+  if (!passwordResult.isValid) {
+    print("Password error: ${passwordResult.errors}");
+  } else {
+    print('Valid password');
+  }
 }
